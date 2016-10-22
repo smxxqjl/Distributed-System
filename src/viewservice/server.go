e3f0334887a9fd8b7722bcf6d9ec39d8549615c5
huxi <smxxqjl@gmail.com>
2016-10-22 14:17:15 +0800

    pass lab2b

--- src/viewservice/server.go
+++ src/viewservice/server.go
@@ -1,4 +1,3 @@
-
 package viewservice
 
 import "net"
@@ -10,22 +9,21 @@ import "fmt"
 import "os"
 
 type ViewServer struct {
-  mu sync.Mutex
-  l net.Listener
-  dead bool
-  me string
+	mu   sync.Mutex
+	l    net.Listener
+	dead bool
+	me   string
 
-
-  // Your declarations here.
-  primary string
-  backup string
-  primaryPingout int
-  backupPingout int
-  primaryCrash bool
-  backupCrash bool
-  ack bool
-  currentView View
-  sendView View
+	// Your declarations here.
+	primary        string
+	backup         string
+	primaryPingout int
+	backupPingout  int
+	primaryCrash   bool
+	backupCrash    bool
+	ack            uint
+	currentView    View
+	sendView       View
 }
 
 //
@@ -33,161 +31,139 @@ type ViewServer struct {
 //
 func (vs *ViewServer) Ping(args *PingArgs, reply *PingReply) error {
 
-  // Your code here.
-  /*
-  if vs.primary == "" {
-    vs.primary = args.Me
-    vs.view.Viewnum++
-  } else if vs.backup == "" {
-    vs.backup = args.Me
-  } else if vs.primary == args.Me {
-    vs.mu.Lock()
-    vs.primaryPingout = 0
-    vs.mu.Unlock()
-    if vs.primaryCrash {
-      vs.primary = ""
-      vs.primaryCrash = false
-      vs.primary = vs.backup
-      vs.view.Viewnum++
-    }
-    if vs.backupCrash {
-      vs.backup = ""
-      vs.backupCrash = false
-      vs.view.Viewnum++
-    }
-    if (len(vs.view.Backup) == 0) && (len(vs.backup) >= 0) {
-      reply.View.Backup = vs.backup
-      vs.view.Viewnum++
-    }
-  } else if vs.backup == args.Me {
-    vs.mu.Lock()
-    vs.backupPingout = 0
-    vs.mu.Unlock()
-  }
-  reply.View.Primary = vs.primary
-  reply.View.Viewnum = vs.view.Viewnum
-  vs.view = reply.View
-  log.Printf("%s %s %s %s\n", args.Me, vs.primary, reply.View.Primary, vs.view.Primary)
-  return nil
-  */
+	// Your code here.
 
-  // New code here
-  /*
-  log.Printf(" ")
-  log.Printf("A ping: primary:%s backup:%s ping:%s\n", vs.view.Primary, vs.view.Backup, args.Me)
-  if vs.view.Primary == args.Me {
-    log.Printf("Primary ping\n")
-    vs.ack = true
-    vs.mu.Lock()
-    vs.primaryPingout = 0
-    vs.mu.Unlock()
-    if vs.primaryCrash {
-      log.Printf("I am here\n")
-      vs.ack = false
-      if vs.view.Backup != "" {
-	vs.view.Primary = vs.view.Backup
-	vs.view.Backup = args.Me
-      } else {
-	vs.view.Primary = args.Me
-      }
-      vs.view.Viewnum++
-      vs.primaryCrash = false
-    } else if vs.backupCrash {
-      vs.ack = false
-      vs.view.Backup = ""
-      vs.view.Viewnum++
-      vs.backupCrash = false
-    }
-  } else if vs.view.Primary == "" {
-    log.Printf("Set up primary\n")
-    vs.ack = false
-    vs.nowview.Primary = args.Me
-    vs.nowview.Viewnum++
-    vs.view = vs.nowview
-  } else if vs.backup == args.Me {
-    log.Printf("bakcup ping")
-    vs.mu.Lock()
-    vs.backupPingout = 0
-    vs.mu.Unlock()
-  } else if vs.backup {
-    log.Printf("set new backup")
-    vs.ack = false
-    vs.nowview.Backup = args.Me
-    vs.nowview.Viewnum++
-  } 
-  reply.View = vs.view
-  return nil
-  */
-  updateView := false
+	/* The latter condition is for init condition */
+	//log.Printf("---------------- %d %d\n", args.Viewnum, args.Viewnum)
+	if args.Viewnum == 0 && vs.currentView.Viewnum != 0 {
+		//log.Printf("Zero depth ping !!!!!!!!!!!!!!!!")
+		if args.Me == vs.currentView.Primary {
+			vs.primaryCrash = true
+		} else if args.Me == vs.currentView.Backup {
+			vs.backupCrash = true
+		}
+	}
+	if args.Me == vs.currentView.Primary && args.Viewnum > vs.ack {
+		vs.ack = args.Viewnum
+	}
+	if args.Me == vs.currentView.Primary {
+		vs.mu.Lock()
+		vs.primaryPingout = 0
+		vs.mu.Unlock()
+	} else if args.Me == vs.currentView.Backup {
+		vs.mu.Lock()
+		vs.backupPingout = 0
+		vs.mu.Unlock()
+	}
+	/* In the case that the view has been acknowledged
+	/* View can be updated
+	/* since the vs.ack and vs.currentView.Viewnum are both
+	/* zero in the beginning the init case shall fall into
+	/* this branch too.*/
+	if vs.ack == vs.currentView.Viewnum {
+		updateView := false
+		if vs.backupCrash {
+			vs.currentView.Backup = ""
+			vs.mu.Lock()
+			vs.backupPingout = 0
+			vs.mu.Unlock()
+			updateView = true
+		}
+		if vs.primaryCrash {
+			vs.currentView.Primary = vs.currentView.Backup
+			vs.currentView.Backup = ""
+			vs.mu.Lock()
+			vs.primaryPingout = 0
+			vs.mu.Unlock()
+			updateView = true
+		}
+		if vs.currentView.Primary == "" && !vs.primaryCrash {
+			vs.currentView.Primary = args.Me
+			updateView = true
+		} else if vs.currentView.Backup == "" && args.Me != vs.currentView.Primary {
+			vs.currentView.Backup = args.Me
+			updateView = true
+		}
+		vs.backupCrash = false
+		vs.primaryCrash = false
+		if updateView {
+			vs.currentView.Viewnum++
+		}
+	}
+	reply.View = vs.currentView
+	//log.Printf("View num vs.currentView.Viewnum %d\n", vs.currentView.Viewnum)
 
-  log.Printf("A ping: args:Me:%s primary:%s backup:%s\n", args.Me, vs.currentView.Primary, vs.currentView.Backup)
-  if args.Viewnum == 0 {
-    log.Printf("Zero depth ping !!!!!!!!!!!!!!!!")
-    if args.Me == vs.currentView.Primary {
-      vs.primaryCrash = true
-    } else if args.Me == vs.currentView.Backup {
-      vs.backupCrash = false
-    }
-  }
+	/*
+	  updateView := false
 
-  if vs.backupCrash {
-    log.Printf("Handle backupCrash\n")
-    vs.currentView.Backup = ""
-    updateView = true
-    vs.backupCrash = false
-    vs.mu.Lock()
-    vs.backupPingout = 0
-    vs.mu.Unlock()
-  }
-  if vs.primaryCrash {
-    log.Printf("Handle primaryCrash\n")
-    vs.currentView.Primary = vs.currentView.Backup
-    vs.currentView.Backup = ""
-    updateView = true
-    vs.primaryCrash = false
-    vs.mu.Lock()
-    vs.primaryPingout = 0
-    vs.mu.Unlock()
-  }
-  if args.Me == vs.currentView.Primary {
-    vs.mu.Lock()
-    vs.primaryPingout = 0
-    vs.mu.Unlock()
-  } else if args.Me == vs.currentView.Backup {
-    vs.mu.Lock()
-    vs.backupPingout = 0
-    vs.mu.Unlock()
-  } else if vs.currentView.Primary == "" {
-    vs.currentView.Primary = args.Me
-    updateView = true
-  } else if vs.currentView.Backup == "" {
-    vs.currentView.Backup = args.Me
-    updateView = true
-  }
-  if updateView {
-    //log.Printf("Add new num")
-    vs.currentView.Viewnum++
-  }
+	  log.Printf("A ping: args:Me:%s primary:%s backup:%s\n", args.Me, vs.currentView.Primary, vs.currentView.Backup)
+	  if args.Viewnum == 0 {
+	    log.Printf("Zero depth ping !!!!!!!!!!!!!!!!")
+	    if args.Me == vs.currentView.Primary {
+	      vs.primaryCrash = true
+	    } else if args.Me == vs.currentView.Backup {
+	      vs.backupCrash = false
+	    }
+	  }
 
-  if args.Me == vs.sendView.Primary || vs.sendView.Primary == "" {
-    vs.sendView = vs.currentView
-  }
-  log.Printf("Send view: primary:%s backup:%s\n", vs.sendView.Primary, vs.sendView.Backup)
-  reply.View = vs.sendView
-  return nil
+	  if vs.backupCrash {
+	    log.Printf("Handle backupCrash\n")
+	    vs.currentView.Backup = ""
+	    updateView = true
+	    vs.backupCrash = false
+	    vs.mu.Lock()
+	    vs.backupPingout = 0
+	    vs.mu.Unlock()
+	  }
+	  if vs.primaryCrash {
+	    log.Printf("Handle primaryCrash\n")
+	    vs.currentView.Primary = vs.currentView.Backup
+	    vs.currentView.Backup = ""
+	    updateView = true
+	    vs.primaryCrash = false
+	    vs.mu.Lock()
+	    vs.primaryPingout = 0
+	    vs.mu.Unlock()
+	  }
+	  if args.Me == vs.currentView.Primary {
+	    vs.mu.Lock()
+	    vs.primaryPingout = 0
+	    vs.mu.Unlock()
+	  } else if args.Me == vs.currentView.Backup {
+	    vs.mu.Lock()
+	    vs.backupPingout = 0
+	    vs.mu.Unlock()
+	  } else if vs.currentView.Primary == "" {
+	    vs.currentView.Primary = args.Me
+	    updateView = true
+	  } else if vs.currentView.Backup == "" {
+	    vs.currentView.Backup = args.Me
+	    updateView = true
+	  }
+	  if updateView {
+	    //log.Printf("Add new num")
+	    vs.currentView.Viewnum++
+	  }
+
+	  if args.Me == vs.sendView.Primary || vs.sendView.Primary == "" {
+	    vs.sendView = vs.currentView
+	  }
+	  log.Printf("Send view: primary:%s backup:%s\n", vs.sendView.Primary, vs.sendView.Backup)
+	  reply.View = vs.sendView
+	*/
+	return nil
 }
 
-// 
+//
 // server Get() RPC handler.
 //
 func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 
-  // Your code here.
-  reply.View = vs.currentView
-  log.Printf("currentView: %s %s\n", vs.currentView.Primary, vs.currentView.Backup)
-  return nil
+	// Your code here.
+	reply.View = vs.currentView
+	//log.Printf("currentView: %s %s\n", vs.currentView.Primary, vs.currentView.Backup)
+	return nil
 }
-
 
 //
 // tick() is called once per PingInterval; it should notice
@@ -196,24 +172,24 @@ func (vs *ViewServer) Get(args *GetArgs, reply *GetReply) error {
 //
 func (vs *ViewServer) tick() {
 
-  // Your code here.
+	// Your code here.
 
-  vs.mu.Lock()
-  if vs.currentView.Primary != "" {
-    vs.primaryPingout++
-  }
-  if vs.currentView.Backup != "" {
-    vs.backupPingout++
-  }
-  vs.mu.Unlock()
-  
-  if vs.primaryPingout >= DeadPings {
-    log.Printf("Set primaryCrash\n")
-    vs.primaryCrash = true
-  } else if vs.backupPingout >= DeadPings {
-    log.Printf("Set Backup Crash\n")
-    vs.backupCrash = true
-  }
+	vs.mu.Lock()
+	if vs.currentView.Primary != "" {
+		vs.primaryPingout++
+	}
+	if vs.currentView.Backup != "" {
+		vs.backupPingout++
+	}
+	vs.mu.Unlock()
+
+	if vs.primaryPingout >= DeadPings {
+		//log.Printf("Set primaryCrash\n")
+		vs.primaryCrash = true
+	} else if vs.backupPingout >= DeadPings {
+		//log.Printf("Set Backup Crash\n")
+		vs.backupCrash = true
+	}
 }
 
 //
@@ -222,61 +198,61 @@ func (vs *ViewServer) tick() {
 // please don't change this function.
 //
 func (vs *ViewServer) Kill() {
-  vs.dead = true
-  vs.l.Close()
+	vs.dead = true
+	vs.l.Close()
 }
 
 func StartServer(me string) *ViewServer {
-  vs := new(ViewServer)
-  vs.me = me
-  // Your vs.* initializations here.
-  vs.primary = ""
-  vs.backup = ""
-  vs.primaryPingout = 0
-  vs.backupPingout = 0
-  vs.primaryCrash = false
-  vs.backupCrash = false
-  vs.ack = true
+	vs := new(ViewServer)
+	vs.me = me
+	// Your vs.* initializations here.
+	vs.primary = ""
+	vs.backup = ""
+	vs.primaryPingout = 0
+	vs.backupPingout = 0
+	vs.primaryCrash = false
+	vs.backupCrash = false
+	vs.ack = 0
 
-  // tell net/rpc about our RPC server and handlers.
-  rpcs := rpc.NewServer()
-  rpcs.Register(vs)
+	// tell net/rpc about our RPC server and handlers.
+	rpcs := rpc.NewServer()
+	rpcs.Register(vs)
 
-  // prepare to receive connections from clients.
-  // change "unix" to "tcp" to use over a network.
-  os.Remove(vs.me) // only needed for "unix"
-  l, e := net.Listen("unix", vs.me);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  vs.l = l
+	// prepare to receive connections from clients.
+	// change "unix" to "tcp" to use over a network.
+	os.Remove(vs.me) // only needed for "unix"
+	l, e := net.Listen("unix", vs.me)
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+	vs.l = l
 
-  // please don't change any of the following code,
-  // or do anything to subvert it.
+	// please don't change any of the following code,
+	// or do anything to subvert it.
 
-  // create a thread to accept RPC connections from clients.
-  go func() {
-    for vs.dead == false {
-      conn, err := vs.l.Accept()
-      if err == nil && vs.dead == false {
-        go rpcs.ServeConn(conn)
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && vs.dead == false {
-        fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
-        vs.Kill()
-      }
-    }
-  }()
+	// create a thread to accept RPC connections from clients.
+	go func() {
+		for vs.dead == false {
+			conn, err := vs.l.Accept()
+			if err == nil && vs.dead == false {
+				go rpcs.ServeConn(conn)
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && vs.dead == false {
+				fmt.Printf("ViewServer(%v) accept: %v\n", me, err.Error())
+				vs.Kill()
+			}
+		}
+	}()
 
-  // create a thread to call tick() periodically.
-  go func() {
-    for vs.dead == false {
-      vs.tick()
-      time.Sleep(PingInterval)
-    }
-  }()
+	// create a thread to call tick() periodically.
+	go func() {
+		for vs.dead == false {
+			vs.tick()
+			time.Sleep(PingInterval)
+		}
+	}()
 
-  return vs
+	return vs
 }
